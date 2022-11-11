61fc8883e3456cff118cfa42401cc1e2f26db1b2
MB-teamdevops <teamdevops@mindbowser.com>
2022-11-11 18:54:23 +0530

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,119 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Ashwinkumar Kawade",
+    "role": "DevOps Engineer",
+    "linkedinId":"linkedin.com/in/ashwinkumar-kawade/",
+    "skypeid": "live:.cid.3c22adfc840aa556",
+    "roleDescription": " Over 3+ years of extensive experience in architecting, automating and optimizing mission critical deployments over large infrastructure. Proficient with Configuration Management tools, and in developing CI/ CD pipelines.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/ashwinkumar-kawade/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/Ashwin-303",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://join.skype.com/invite/vyPNQvcAUxWW",
+          "className":"fa fa-skype"
+        }
+      ],
+    "aboutme":"Ever since I was a kid I have been driven by intellectual curiosity to find answer to problems, Also it drives me to be a lifelong learner.With my refined skill set, I can bring special value to my work as a great team member.Having hands-on experience in various fields of Engineering",
+    "address":"India",
+    "website":"https://",
+    "education":[
+      {
+        "UniversityName":"Savitribai Phule Pune University, Pune",
+        "specialization":"Bachelor of Engineering (Electronics and Tele-Communication)",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2020",
+        "Achievements":""
+      }
+      // {
+      //   "UniversityName":"Some University",
+      //   "specialization":"Some specialization",
+      //   "MonthOfPassing":"Jan",
+      //   "YearOfPassing":"2018",
+      //   "Achievements":"Some Achievements"
+      // }
+    ],
+    "work":[
+      {
+        "CompanyName":"Mindbowser Info Solutions Pvt. Ltd, Pune",
+        "specialization":"DevOps Engineer",
+        "MonthOfLeaving":"Current",
+        "YearOfLeaving":"",
+        "Achievements":""
+      },
+      {
+        "CompanyName":"DentalDost, Baner, Pune",
+        "specialization":"DevOps Engineer",
+        "MonthOfLeaving":"Dec",
+        "YearOfLeaving":"2020",
+        "Achievements":""
+      },
+      {
+        "CompanyName":"OpsEco Technologies LLP, Baner, Pune",
+        "specialization":"Jr. DevOps Engineer",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2020",
+        "Achievements":""
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"AWS"
+      },
+      {
+        "skillname":"Terraform"
+      },
+      {
+        "skillname":"Kubernetes"
+      },
+      {
+        "skillname":"Jenkins"
+      },
+      {
+        "skillname":"Ansible"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Microservice application on AWS EKS",
+        "description":"I was involved in designing, Planning, Infra Provisioning, CI/CD Pipeline, and Monitoring",
+        "imgurl":"images/portfolio/microservices.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
