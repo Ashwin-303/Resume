b589b75cdd80cc6afd33acc29dd6753b5d03d791
MB-teamdevops <teamdevops@mindbowser.com>
2022-11-13 13:37:18 +0530

    Updates

---
+++ precache-manifest.226d4c888c9694963bc38d3349e2dbb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "10c2c72e77ee7de6471e",
+    "url": "/Resume/static/css/main.e7f21f20.chunk.css"
+  },
+  {
+    "revision": "10c2c72e77ee7de6471e",
+    "url": "/Resume/static/js/main.10c2c72e.chunk.js"
+  },
+  {
+    "revision": "499bf8bcb9159cbc401f",
+    "url": "/Resume/static/js/1.499bf8bc.chunk.js"
+  },
+  {
+    "revision": "8d5efb982c82b6672434",
+    "url": "/Resume/static/js/runtime~main.8d5efb98.js"
+  },
+  {
+    "revision": "1a6400553e1852a22d6d4b7e34faa3aa",
+    "url": "/Resume/static/media/HEXAGONAL_GRID.1a640055.mp4"
+  },
+  {
+    "revision": "600352300b5343213760cf945a924444",
+    "url": "/Resume/index.html"
+  }
+];
